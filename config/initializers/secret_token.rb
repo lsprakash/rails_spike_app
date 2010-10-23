734d4a64cffcf51dbc0c81525e1faf64bc989040
Prakash <prakash.subramaniam@gmail.com>
2010-10-24 00:59:50 +0530

    Initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsSpikeApp::Application.config.secret_token = 'a93063c316dc919703f7a077e109859086055f48d093af3641804e4818a6f6eb8c0aa368aa853bbcef1ad34218bcf36369c015cf5b6fd70961e52f7d18c7b465'
